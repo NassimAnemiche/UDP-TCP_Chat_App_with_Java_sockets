9391b50a05c9b65664a7b6f071287a0459232b85
Nass951 <nanemiche@gmail.com>
2025-11-12 14:11:06 +0100

    update

--- Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Main placeholder - project ready. Run UDPServer to start the UDP listener.");
-    }
-}
